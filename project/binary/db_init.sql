544a6cad1432dc9785cdf938db898267605a6128
jwfing <jwfing@gmail.com>
2013-03-06 13:34:04 +0800

    add db init sql

--- project/binary/db_init.sql
+++ project/binary/db_init.sql
@@ -1,12 +1,35 @@
-CREATE TABLE IF NOT EXISTS CommonDocument (
-   id int not null auto_increment,
-   urlMd5 varchar(64) not null unique,
-   sourceUrl varchar(256) not null,
-   url varchar(256) not null,
-   title varchar(256),
-   channel varchar(256),
-   downloadTime bigint not null,
-   articleText varchar(2048),
-   description varchar(1024),
-   PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS `zb_followlink` (
+      `id` bigint(20) NOT NULL AUTO_INCREMENT,
+      `comments` varchar(255) DEFAULT NULL,
+      `seedUrl` text NOT NULL,
+      `tags` varchar(255) DEFAULT NULL,
+      `timeCreated` bigint(20) DEFAULT NULL,
+      `url` text NOT NULL,
+      `urlMd5` varchar(64) NOT NULL,
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `urlMd5` (`urlMd5`),
+      KEY `tags` (`tags`,`timeCreated`,`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zb_category` (
+      `id` bigint(20) NOT NULL AUTO_INCREMENT,
+      `tag` varchar(64) NOT NULL,
+      `samples` text,
+      `time_created` bigint(20) DEFAULT NULL,
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `tag` (`tag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS`zb_seed` (
+      `id` bigint(20) NOT NULL AUTO_INCREMENT,
+      `next_fetch` bigint(20) DEFAULT NULL,
+      `strict` text,
+      `tags` varchar(255) DEFAULT NULL,
+      `time_created` bigint(20) DEFAULT NULL,
+      `update_period` bigint(20) DEFAULT NULL,
+      `url` text NOT NULL,
+      `urlMd5` varchar(64) NOT NULL,
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `urlMd5` (`urlMd5`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
